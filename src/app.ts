9ac2184937af2bf93bdb7d7c509ab41fad3ce3a0
Jakub Hypki <37691329+jhypki@users.noreply.github.com>
2025-01-21 23:25:44 +0100

    buzzer endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { espRoutes } from './routes/espRoutes';
 import path from 'path';
 import cors from 'cors';
 import { imagesRoutes } from './routes/imagesRoutes';
+import { buzzerStateRoutes } from './routes/buzzerStateRoutes';
 import { activationsRoutes } from './routes/activationsRoutes';
 import errorHandler from './middleware/error-handler';
 import { authenticationMiddleware } from './middleware/authenticationMiddleware';
@@ -17,6 +18,7 @@ app.use(authenticationMiddleware);
 app.use('/esp', espRoutes);
 app.use('/images', imagesRoutes);
 app.use('/activations', activationsRoutes);
+app.use('/buzzer-state', buzzerStateRoutes);
 app.use(
     '/uploads',
     express.static(path.join(__dirname, '../uploads'), {
